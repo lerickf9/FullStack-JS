0bcec43f20ede9738a492b15c4e04d5d0e7167ee
Erick <efuentes.edf9@gmail.com>
2024-08-16 23:14:15 -0400

    Add folder helpers

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import generarId from '../helpers/generarid.js'
 
 const veterinarioSchema = mongoose.Schema({
     nombre: {
@@ -27,7 +28,7 @@ const veterinarioSchema = mongoose.Schema({
     },
     token:{
         type: String,
-        default: Date.now(),
+        default: generarId(),
     },
     confirmado: {
         type: Boolean,
