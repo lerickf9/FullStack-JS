10b55a913b08368ee903badebb6f1e691fbb54b3
Erick <efuentes.edf9@gmail.com>
2024-08-28 22:28:36 -0400

    Doc. helpers/generarJWT

--- backend/helpers/generarJWT.js
+++ backend/helpers/generarJWT.js
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken';
 
+
+//JWT_SECRET gneramos una palabra clave para que sea más dificil de detectar o crakear el token
 const generarJWT = (id) =>{
 
+    //El metodo sign envia 2 parametros de entrada. 1 es nombre de la variable que se quedará guardado y el segundo la palabra clave
     return jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn: "30d",
 
