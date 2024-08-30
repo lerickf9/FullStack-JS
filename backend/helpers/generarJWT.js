2857c1b099cb20c73dfff21e51ad2f64d52580d2
Erick <efuentes.edf9@gmail.com>
2024-08-29 22:08:37 -0400

    Doc. middlewere function checkAuth para el token de cada perfil

--- backend/helpers/generarJWT.js
+++ backend/helpers/generarJWT.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 //JWT_SECRET gneramos una palabra clave para que sea más dificil de detectar o crakear el token
 const generarJWT = (id) =>{
 
-    //El metodo sign envia 2 parametros de entrada. 1 es nombre de la variable que se quedará guardado y el segundo la palabra clave
+    //El metodo sign envia 2 parametros de entrada. 1 es el id del usuario que se quedará guardado y el segundo la palabra clave
     return jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn: "30d",
 
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,28 +5,21 @@ const checkAuth = async (req, res, next) => {
     let token; 
     console.log('Desde mi Middleware');
 
+    //comprobamos que el usuario envia el token en la cabecera. el stasrtWith pide el token venga con 'Bearer'
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-
+        console.log("Si tiene un token con bearer");
     }
-
     try{
+
         token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado");
-
         return next()
     }catch(error){
         const e = new Error ('Token no valido');
         return res.status(403).json({msg: e.message})
     }
     
-    if(!token){
-        const error = new Error ('Token no valido o inexistente');
-        res.status(403).json({msg: error.message})
-    }
-    
-    
-    next();
 };
 
 export default checkAuth;
