bcb3ea671e32084cabbac5a036e0d65f066876ab
Erick <efuentes.edf9@gmail.com>
2024-08-29 22:23:19 -0400

    Doc. Funciton finished with Token valided

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,10 +10,16 @@ const checkAuth = async (req, res, next) => {
         console.log("Si tiene un token con bearer");
     }
     try{
-
+        //Eliminamos el bearer con el split cuando encunetro el primer espacio y devuelve el token completo
         token = req.headers.authorization.split(' ')[1];
+        //verify pide 2 parametro el primeros es el token enviado en el servidor y la siguiente es la palabra clave
+        //Asi el sistema tiene certeza que el usuario a sido autenticado.
+        //Luego el objeto decoded tiene las atributos del Veterinario y por puede acceder a su id
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        //el select - m,e arroja la respuestas menos las otras variables en este caso solo es id email y telefono
+        //incoporamos reques a veterinario para crear la sesion con un Veterinario.
         req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado");
+        //Si el codigo es correcto pasara al siguiente midlewere con next()
         return next()
     }catch(error){
         const e = new Error ('Token no valido');
